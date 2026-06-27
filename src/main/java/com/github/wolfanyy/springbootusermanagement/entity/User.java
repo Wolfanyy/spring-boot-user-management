b0568854c6b87bd09538911a2bef30dcb87c42ee
Wolfanyy <gribanova.anna98@mail.ru>
2026-06-27 20:42:09 +0400

    feat: add user entity

---
+++ src/main/java/com/github/wolfanyy/springbootusermanagement/entity/User.java
@@ -0,0 +1,40 @@
+package com.github.wolfanyy.springbootusermanagement.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 30)
+    private String name;
+
+    @Column(name = "last_name", nullable = false, length = 30)
+    private String lastName;
+
+    @Column(
+            name = "email",
+            nullable = false,
+            unique = true,
+            length = 50
+    )
+    private String email;
+
+    @Column(name = "age", nullable = false)
+    private Integer age;
+}
