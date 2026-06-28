27c3a47879b58fefa69a26efc37523c083704893
Wolfanyy <gribanova.anna98@mail.ru>
2026-06-28 17:40:22 +0400

    feat: add findByEmail method to user repository

--- src/main/java/com/github/wolfanyy/springbootusermanagement/repository/UserRepository.java
+++ src/main/java/com/github/wolfanyy/springbootusermanagement/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.github.wolfanyy.springbootusermanagement.repository;
 import com.github.wolfanyy.springbootusermanagement.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
 }
