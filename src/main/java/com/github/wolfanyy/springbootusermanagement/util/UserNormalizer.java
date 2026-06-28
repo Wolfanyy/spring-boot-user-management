5faa208a6734410db000555c1eec68f367a6fffc
Wolfanyy <gribanova.anna98@mail.ru>
2026-06-28 23:53:31 +0400

    feat: add user normalizer

---
+++ src/main/java/com/github/wolfanyy/springbootusermanagement/util/UserNormalizer.java
@@ -0,0 +1,20 @@
+package com.github.wolfanyy.springbootusermanagement.util;
+
+import com.github.wolfanyy.springbootusermanagement.entity.User;
+import com.github.wolfanyy.springbootusermanagement.exception.ValidationException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserNormalizer {
+
+    public void normalize(User user) {
+
+        if (user == null) {
+            throw new ValidationException("User cannot be null");
+        }
+
+        user.setName(user.getName() != null ? user.getName().trim() : null);
+        user.setLastName(user.getLastName() != null ? user.getLastName().trim() : null);
+        user.setEmail(user.getEmail() != null ? user.getEmail().trim().toLowerCase() : null);
+    }
+}
